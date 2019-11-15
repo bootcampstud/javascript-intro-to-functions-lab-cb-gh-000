16af3e7c7395db00e5eed1e31c66272583166416
Kristin Baday <kristinsbdy@gmail.com>
2019-11-15 17:31:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,15 @@ function shout(something){
     something = something.toUpperCase();
     return(something)
 }
+function whisper(something){
+  something = something.toLowerCase();
+  return(something)
+}
+function logShout(something){
+  something = something.toUpperCase();
+  return(console.log(something))
+}
+function logWhisper(something){
+  something = something.toLowerCase();
+  return(console.log(something))
+}
